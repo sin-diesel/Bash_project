cc77409b1477192d2fb7859b23cb1dec6dcb0ce0
Stas_SD <sidelnikov.si@phystech.edu>
2020-07-12 15:32:04 +0300

    added file containing info about directory

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,9 @@
+
+function get_lines { # receives name of file as an argument
+	ls -l -G -p > list.txt
+}
+
 echo "Guess how many files are in the current directory?"
 read num
 echo "You entered: $num"
+get_lines
