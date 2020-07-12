ae730ad639468ede1442363818a42b94a91dd16f
Stas_SD <sidelnikov.si@phystech.edu>
2020-07-12 22:46:35 +0300

    script updated, no checks input

--- guessinggame.sh
+++ guessinggame.sh
@@ -7,16 +7,27 @@ function get_lines { # counts the amount of files in current directory using ls 
 answer=$(get_lines)
 num=-1
 
+echo "Guess how many files are in the current directory?"
+
 while [[ ! $num -eq $answer ]]
 do
-echo "Guess how many files are in the current directory?"
+
 read num
-if [[ $num -lt $answer ]]
+
+if [[ ! $num =~ ^[1-9][0-9]*$ ]] && [[ ! $num -eq 0 ]]
 then
-	echo "The answer is greater than the number you entered"
-elif [[ $num -gt $answer ]]
+	echo "Sorry, seems to be incorrect input, try again"
+	num=-1
+fi
+
+if [[ $num -lt $answer ]] && [[ ! $num -eq -1 ]]
 then
-	echo "The answer is less than the number you entered"
+	echo "The answer is greater than the number you entered, try again"
+
+elif [[ $num -gt $answer ]] && [[ ! $num -eq -1 ]]
+
+then
+	echo "The answer is less than the number you entered, try again"
 fi
 done
 
