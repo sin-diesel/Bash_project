5e625fe5bbbc2ce9fbdbee62ee23581d6409e7f4
Stas_SD <sidelnikov.si@phystech.edu>
2020-07-12 15:47:43 +0300

    Now program works fine

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,7 +3,20 @@ function get_lines { # counts the amount of files in current directory using ls 
 	ls -l -G -p | egrep '\-r.*' | wc -l
 }
 
+answer=$(get_lines)
+num=0
+
+while [[ ! $num -eq $answer ]]
+do
 echo "Guess how many files are in the current directory?"
 read num
-answer=$(get_lines)
-echo $answer
+if [[ $num -lt $answer ]]
+then
+	echo "The answer is greater than the number you entered"
+elif [[ $num -gt $answer ]]
+then
+	echo "The answer is less than the number you entered"
+fi
+done
+
+echo "Correct! Congratulations, dear Padawan!"
